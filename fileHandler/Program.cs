e085e290eb20374adedcb27b61180a3df2ea898e
Lucas Machado <lukaasmachado143@gmail.com>
2022-08-16 23:10:15 -0300

    Leitura e escritura em arquivo TXT CONCLUÍDA

--- fileHandler/Program.cs
+++ fileHandler/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace fileHandler
 {
@@ -10,6 +11,31 @@ namespace fileHandler
     {
         static void Main(string[] args)
         {
+            StreamWriter NewArchiveTest = File.AppendText("test.txt");
+            NewArchiveTest.WriteLine("Lucas");
+            NewArchiveTest.WriteLine("Ivana");
+            NewArchiveTest.WriteLine("Teste");
+            NewArchiveTest.Close();
+
+            StreamReader readerArchive = new StreamReader("test.txt");
+            List<string> contentArchive = new List<string>();
+            string linesArchive = "";
+
+            while(linesArchive != null)
+            {
+                linesArchive = readerArchive.ReadLine();
+                if (linesArchive != null)
+                {
+                    contentArchive.Add(linesArchive);
+                }
+            }
+            readerArchive.Close();
+            foreach (string line in contentArchive)
+            {
+                Console.WriteLine(line);
+            }
+            Console.ReadLine();
+            
         }
     }
 }
